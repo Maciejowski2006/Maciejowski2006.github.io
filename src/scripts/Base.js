1cf7133fc9c365480cbd2d22c81f82585077518a
Maciej <maciej@localhost.localdomain>
2022-01-27 16:04:45 +0100

    Initial commit

---
+++ src/scripts/Base.js
@@ -0,0 +1,21 @@
+let header;
+let global;
+
+function LinkTo(link)
+{
+	window.open(link, "_blank");
+}
+function Main()
+{
+	header = document.getElementById("header");
+	global = document.getElementById("container");
+	document.getElementById("title").innerHTML = `<h2>About <span onclick="Reveal()" style="cursor: pointer; user-select: none;">me</h2>`;
+	document.title = "Maciej Chmiel 5 - 4gdgqqy";
+	document.getElementById("youtube").setAttribute("title", "7 - nz5lo7k");
+	document.getElementById("twitter").setAttribute("title", "8 - oczl4yd");
+	console.log("8 - oczl4yd");
+}
+function Reveal()
+{
+	header.innerHTML += ` 6 - jiuzcry`;
+}
